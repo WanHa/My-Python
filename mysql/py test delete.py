89a481520497a463ca7e34d56b0e30af41afea5d
1043346131@qq.com <1043346131@qq.com>
2017-10-13 16:37:30 +0800

    update

--- mysql/py test delete.py
+++ mysql/py test delete.py
@@ -1,6 +1,6 @@
 
 print("haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-
+import pymssql
 import pymysql
 
 db=pymysql.connect(host='127.0.0.1',port=3306,user='wh',password='123456',db='test', charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
