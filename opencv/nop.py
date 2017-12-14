e8187ec22e6631a7a10a8694b9db81b7bb62c3b9
WanHa <1043346131@qq.com>
2017-12-14 09:36:03 +0800

    图片文字识别

--- opencv/nop.py
+++ opencv/nop.py
@@ -1,5 +1,12 @@
-import cv2
+import pytesseract
+from PIL import Image
+path='C:/Users/hao.k.wang/Desktop/python opencv/oldproject/My-Python/img/t5.jpg'
+#tessdata_dir_config='--tessdata-dir "C:\\Program Files\\Tesseract-OCR\\tessdata"'
+image = Image.open(path)
+image.load()  # 加载一下图片，防止报错，此处可省略
+image.show()  # 调用show来展示图片，调试用，可省略
+code = pytesseract.image_to_string(image, lang='chi_sim')
+#code = pytesseract.image_to_string(image, lang='chi_sim', config=tessdata_dir_config)
+#code = pytesseract.image_to_string(image)
 
-img=cv2.imread('')
-cv2.imshow('w',img)
-cv2.waitKey(0)
+print(code)
